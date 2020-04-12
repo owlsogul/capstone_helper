230d627ad39db35affe6e7c96868f6314da4c172
owlsogul <owlsogul@naver.com>
2020-04-13 01:43:25 +0900

    server init

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 80
+
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+app.use(express.json())
+
+// db
+var sequelize = require('./models').sequelize;
+sequelize.sync().then(()=>{
+    console.log("Successfully connec to server")
+}).catch((err)=>{
+    console.log(err)
+});
+
+// router
+//app.use("/api", require("./api"))
+app.get('/', (req, res) => res.send('Hello World!'))
+
+// open
+app.listen(port, () => {
+        console.log(`Example app listening on port ${port}!`)
+    }
+)

247a24b24702bf369931d5f8513f69f13f26637f
owlsogul <owlsogul@naver.com>
2020-04-23 19:20:11 +0900

    swagger 등록

--- backend/api/user/user.controller.js
+++ backend/api/user/user.controller.js
@@ -1,3 +1,45 @@
+/**
+ * @swagger
+ * tags:
+ *   name: User
+ *   description: user 관련 API들
+ * definitions:
+ *   ReqSignIn:
+ *     type: object
+ *     required:
+ *       - user_id
+ *       - password
+ *     properties:
+ *       user_id:
+ *         type: string
+ *         description: 아이디
+ *       password:
+ *         type: string
+ *         description: 비밀번호
+ *   ResSignIn:
+ *     type: object
+ *     required:
+ *       - status
+ *     properties:
+ *       status:
+ *         type: string
+ *         description: 로그인 성공 여부- error, success
+ *       token:
+ *         type: object
+ *         description: 계정 정보
+ *   Response_error:
+ *     type: object
+ *     required:
+ *       - status
+ *     properties:
+ *       message:
+ *         type: string
+ *         description: 오류 사유
+ *       status:
+ *         type: integer
+ *         description: response code
+ */
+
 const models = require("../../models")
 const crypto = require("crypto")
 const coder = require("../../util/coder")
@@ -34,6 +76,41 @@ function makeRandomString(from, length) {
     }
     return randomstring;
 }
+
+/**
+ * @swagger
+ *  paths:
+ *    /login:
+ *      post:
+ *        tags:
+ *        - "User"
+ *        summary: "Login process"
+ *        description: ""
+ *        consumes:
+ *        - "application/json"
+ *        produces:
+ *        - "application/json"
+ *        parameters:
+ *        - in: "body"
+ *          name: "body"
+ *          description: "로그인 계정 정보와 서비스 정보를 전달"
+ *          required: true
+ *          schema:
+ *            $ref: "#/definitions/ReqSignIn"
+ *        responses:
+ *          200:
+ *            description: "로그인 결과"
+ *            schema:
+ *              $ref: "#/definitions/Auth_response"
+ *          400:
+ *            description: "잘못된 데이터"
+ *            schema:
+ *              $ref: "#/definitions/Response_error"
+ *          500:
+ *            description: "로그인 오류 & 실패"
+ *            schema:
+ *              $ref: "#/definitions/Response_error"
+ */
 
 exports.login = (req, res) => {
 
--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,37 @@ const cookieParser = require('cookie-parser');
 app.use(cookieParser(serverConfig.cookieKey));
 app.use(express.json())
 
+// swagger doc
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+
+
+// Swagger definition
+// You can set every attribute except paths and swagger
+// https://github.com/swagger-api/swagger-spec/blob/master/versions/2.0.md
+const swaggerDefinition = {
+  info: { // API informations (required)
+    title: 'Capstone Helper', // Title (required)
+    version: '1.0.0', // Version (required)
+    description: 'Capstone Helper API list' // Description (optional)
+  },
+  basePath: '/' // Base path (optional)
+};
+
+// Options for the swagger docs
+const options = {
+  // Import swaggerDefinitions
+  swaggerDefinition,
+  // Path to the API docs
+  apis: ['./api/user/user.controller.js']
+};
+
+// Initialize swagger-jsdoc -> returns validated swagger spec in json format
+const swaggerSpec = swaggerJSDoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
 
 // middleware
 var cors = require('cors');

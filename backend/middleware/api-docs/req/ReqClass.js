ba8f7cd43880a795deaa1e043c06f585e08cf6a8
owlsogul <owlsogul@naver.com>
2020-04-25 04:55:35 +0900

    class docs

--- backend/middleware/api-docs/req/ReqClass.js
+++ backend/middleware/api-docs/req/ReqClass.js
@@ -1,10 +1,18 @@
 module.exports = {
-    ReqCreateClass: {
-        type: "object",
-        required: [ "className" ],
-        properties: {
-          className: { type: "string", description: "수업의 이름" },
-          classTime: { type: "string", description: "수업 시간"}
-        } 
-      }
+  ReqCreateClass: {
+    type: "object",
+    required: [ "className" ],
+    properties: {
+      className: { type: "string", description: "수업의 이름" },
+      classTime: { type: "string", description: "수업 시간"}
+    } 
+  },
+  ReqInviteAssist: {
+    type: "object",
+    required: [ "classId", "assistants" ],
+    properties: {
+      classId: { type: "integer", description: "조교를 초대할 수업" },
+      assistants: { type: "array", description: "조교들의 이메일" }
+    }
+  }
 }
--- backend/middleware/api-docs/res/ResClass.js
+++ backend/middleware/api-docs/res/ResClass.js
@@ -6,7 +6,21 @@ module.exports = {
       required: [ "className" ],
       properties: {
         className: { type: "string", description: "만들어진 수업의 이름" },
+        classId: { type: "integer", description: "만들어진 수업의 코드"}
       }
     }
-  }
+  },
+
+  ResInviteAssist: {
+    description: "정말로 초대된 조교들만 반환한다. 이미 조교여도 반환된다.",
+    schema: {
+      type: "object",
+      required: [ "className" ],
+      properties: {
+        className: { type: "string", description: "만들어진 수업의 이름" },
+        classId: { type: "integer", description: "만들어진 수업의 코드"}
+      }
+    }
+  },
+
 }

c0020fecf593d90096bd2661238170e780b6b3c7
yejin <yy991125@naver.com>
2020-04-26 21:33:44 +0900

    수업 대기중 상태에 대한 코드 추가

--- frontend/capstone/src/components/RedirectingComponent.js
+++ frontend/capstone/src/components/RedirectingComponent.js
@@ -5,59 +5,79 @@ import { Redirect } from 'react-router-dom';
 // 로그인 되었는지 확인하는 함수
 function userInfo(sendObj) {
   return fetch('/api/user/user_info', {
-      method: 'GET',
-      credentials: 'include'
+    method: 'GET',
+    credentials: 'include'
+  })
+}
+
+function classList() {
+  return fetch('/api/class/list', {
+    method: 'GET',
+    credentials: 'include'
   })
 }
 
 export default class RedirectingComponent extends Component {
   state = {
-    isLogin: false, 
+    isLogin: false,
     isWaiting: false
   }
 
-  componentWillMount(){ 
+  componentWillMount() {
     // render() 전에 call 됨. 
-    userInfo().then((response)=> response.json())
-    .then((json)=>{
-      console.log("response 받아오기 성공~!~!");
-      console.log(json);
-      if (json["err"] == null || json["err"] == undefined){
-        // 성공
-        // 승인 대기 수업일때 넘어가는 화면으로 가기
-          this.setState({
-            isLogin: true, 
-            isWaiting: true
-          })
+    userInfo().then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        if (json["err"] == null || json["err"] == undefined) {
+          // 성공
+          classList()
+            .then((response) => response.json())
+            .then((json) => {
+              console.log(json)
+              json["take"].forEach(each => {
+                if (each["takeStatus"] == 0) {
+                  // 대기중인 수업
+                  this.setState({
+                    isLogin: true,
+                    isWaiting: true
+                  })
+                  return;
+                }
+              })
+            })
+            .then(
+              // 승인 대기 수업일때 넘어가는 화면으로 가기
+              this.setState({
+                isLogin: true,
+                isWaiting: true
+              })
+            )
+        }
         // 아니면 메인으로 가기
-      } else {
-        console.log(json["err"]);
-        // 실패 (로그인 상태 아님)
-        // TODO: 로그인 화면으로 이동
-        this.setState({
-          isLogin: false, 
-          isWaiting: false
-        })
-      }
-    })
-    .catch((err)=>{
-      console.log(err);
-    })
+        else {
+          console.log(json["err"]);
+          // 실패 (로그인 상태 아님)
+          this.setState({
+            isLogin: false,
+            isWaiting: false
+          })
+        }
+      })
   }
-  
+
   render() {
     const { isLogin, isWaiting } = this.state;
     if (isLogin && isWaiting) {
       // 기다리는중
-      return <Redirect to='/wait'/>;
+      return <Redirect to='/wait' />;
     }
-    else if (isLogin && !isWaiting){
+    else if (isLogin && !isWaiting) {
       // 로그인
-      return <Redirect to='/sign-in'/>;
+      return <Redirect to='/sign-in' />;
     }
     else {
       // 메인
-      return <Redirect to='/main'/>
+      return <Redirect to='/main' />
     }
   }
 }

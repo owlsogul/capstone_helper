a83240c2ac91540eae2eba448fa545f977463522
minsu2344 <50162544+minsu2344@users.noreply.github.com>
2020-05-04 01:23:47 +0900

    Update App.js

--- frontend/capstone/src/App.js
+++ frontend/capstone/src/App.js
@@ -4,39 +4,31 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
+import NavigationBar from './components/NavigationBar';
 import Login from './components/login.component';
 import SignUp from './components/signup.component';
+
+import wait from './components/wait';
+import OpenClass from './components/openclass.componet';
+import RedirectingComponent from './components/RedirectingComponent.js';
 
 
 function App () {
   return (<Router>
     <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-        <div className="contatiner">
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to={"login"}>로그인</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"signup"}>회원가입</Link>
-              </li>
-            </ul>
-          </div>
+        <NavigationBar></NavigationBar>
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Switch>
+              <Route exact path='/' component={RedirectingComponent} />
+              <Route path="/sign-in" component={Login} />
+              <Route path="/sign-up" component={SignUp} />
+              <Route path="/wait" component={wait} />
+              <Route path="/open-class" component={OpenClass} />
+            </Switch>
+            </div>
         </div>
-      </nav>
-
-
-      <div className="auth-wrapper">
-        <div className="auth-inner">
-          <Switch>
-            <Route exact path='/' component={Login} />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={SignUp} />
-          </Switch>
         </div>
-      </div>
-    </div> 
     </Router>
   )
 }

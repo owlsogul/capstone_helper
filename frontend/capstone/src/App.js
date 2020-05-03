8b5e900f90ef3c6e609244522aeb4f163e67405b
yejin <yy991125@naver.com>
2020-05-04 00:25:52 +0900

    proxy 바꿈, Notice 컴포넌트 만듦, semantic-ui-react 추가

--- frontend/capstone/src/App.js
+++ frontend/capstone/src/App.js
@@ -3,14 +3,13 @@ import './App.css';
 //import PropTypes from "prop-types";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-
 import NavigationBar from './components/NavigationBar';
-
 import Login from './components/login.component';
 import SignUp from './components/signup.component';
 import wait from './components/wait';
 import OpenClass from './components/openclass.componet';
 import RedirectingComponent from './components/RedirectingComponent.js';
+import Notice from './components/notice.component.js';
 
 function App() {
   return (
@@ -25,6 +24,7 @@ function App() {
               <Route path="/sign-up" component={SignUp} />
               <Route path="/wait" component={wait} />
               <Route path="/open-class" component={OpenClass} />
+              <Route path="/notice" component={Notice} />
             </Switch>
           </div>
         </div>

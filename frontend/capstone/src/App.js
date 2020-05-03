fc9c65bb81a63d2d764d2b46887d22fd126fecd2
minsu2344 <50162544+minsu2344@users.noreply.github.com>
2020-05-03 22:44:39 +0900

    App.js 오류 수정

--- frontend/capstone/src/App.js
+++ frontend/capstone/src/App.js
@@ -4,9 +4,8 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
-import Login from './login.component';
-import SignUp from './signup.component';
-import Sidebar from './sidebar'
+import Login from './components/login.component';
+import SignUp from './components/signup.component';
 
 
 function App () {
@@ -17,10 +16,10 @@ function App () {
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <Link className="nav-link" to={"/sign-in"}>로그인</Link>
+                <Link className="nav-link" to={"login"}>로그인</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to={"/sign-up"}>회원가입</Link>
+                <Link className="nav-link" to={"signup"}>회원가입</Link>
               </li>
             </ul>
           </div>
@@ -32,8 +31,8 @@ function App () {
         <div className="auth-inner">
           <Switch>
             <Route exact path='/' component={Login} />
-            <Route path="/sign-in" component={Login} />
-            <Route path="/sign-up" component={SignUp} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={SignUp} />
           </Switch>
         </div>
       </div>

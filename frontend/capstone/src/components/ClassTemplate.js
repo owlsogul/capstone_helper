8028d78c2e4ca14d1840cb8e56584970a6a50e02
owlsogul <owlsogul@naver.com>
2020-05-11 06:40:24 +0900

    ClassTemplate padding 줌

--- frontend/capstone/src/components/ClassTemplate.js
+++ frontend/capstone/src/components/ClassTemplate.js
@@ -18,7 +18,7 @@ export default class ClassTemplate extends Component {
         <div>
           <Bar match={this.props.match}/>
         </div>
-        <div>
+        <div style={{ padding: 10 }}>
           {this.props.children}
         </div>
       </Dashboard>

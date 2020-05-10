069184aaf5da55c9793164adb8552cd5a47ef6a5
owlsogul <owlsogul@naver.com>
2020-05-11 03:15:17 +0900

    인증 못받으면 못들어가게

--- frontend/capstone/src/pages/DashboardPage.js
+++ frontend/capstone/src/pages/DashboardPage.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Alert } from "react-bootstrap"
+
 import Dashboard from "../components/Dashboard.component"
 
 const style = {
@@ -50,6 +52,11 @@ class ClassCreateButton extends Component {
 class ClassElement extends Component {
   constructor(props){
     super(props)
+    this.onClick = this.onClick.bind(this)
+  }
+
+  onClick(){
+    this.props.handleEnter(this.props.classId, this.props.classType)
   }
 
   render(){
@@ -61,7 +68,7 @@ class ClassElement extends Component {
           <p className="card-text">
             {createClassType(this.props.classType)}
           </p>
-          <a href="#" className="btn btn-primary">교실 들어가기</a>
+          <a href="#" className="btn btn-primary" onClick={this.onClick}>교실 들어가기</a>
         </div>
         
         
@@ -102,6 +109,7 @@ class ClassList extends Component{
                 classId={e.classId}
                 className={e.className}
                 classType={e.classType}
+                handleEnter={this.props.handleEnter}
               />
             )
           })
@@ -115,11 +123,19 @@ class ClassList extends Component{
 export default class DashboardPage extends Component {
   constructor(props) {
     super(props);
-    this.state = { classes: [], isProfessor: false }
+    this.state = { classes: [], isProfessor: false, noAuthAlert: false }
+    this.handleEnter = this.handleEnter.bind(this)
+    this.closeAlert = this.closeAlert.bind(this)
+  }
+
+  handleEnter(classId, classType){
+    console.log(classType)
+    if (classType == "wait"){
+      this.setState({ noAuthAlert: true })
+    }
   }
 
   componentDidMount(){
-    console.log("mount")
     loadClass().then(res=>res.json())
       .then(res=>{
         console.log(res)
@@ -155,13 +171,34 @@ export default class DashboardPage extends Component {
       })
   }
 
+  closeAlert(){
+    this.setState({noAuthAlert: false})
+  }
+
   render() {
+    let alert = <></>
+    if (this.state.noAuthAlert){
+        alert = <Alert 
+          show={this.state.noAuthAlert} 
+          onClose={()=>{ this.closeAlert() }}
+          variant="danger"
+          dismissible
+        >
+          <Alert.Heading>당신은 아직 승인받지 못했습니다.</Alert.Heading>
+          <p>
+            초대링크로 수업에 들어갔으나, 관리자가 승인을 하기 전입니다.
+            계속해서 이 메시지가 나온다면, 새로 고침을 누르거나 수업 관리자에게 연락해보세요.
+          </p>
+        </Alert>
+    }
     return (
       <Dashboard initState={[false, true, false]} history={this.props.history}>
         <ClassList 
           classes={this.state.classes} 
           isProfessor={this.state.isProfessor} 
+          handleEnter={this.handleEnter}
         />
+        {alert}
       </Dashboard>
     );
   }

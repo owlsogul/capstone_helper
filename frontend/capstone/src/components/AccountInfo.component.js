9491fcf4173a3a4387dfc5b0871de553cbf42f2e
owlsogul <owlsogul@naver.com>
2020-05-11 03:14:51 +0900

    로그아웃

--- frontend/capstone/src/components/AccountInfo.component.js
+++ frontend/capstone/src/components/AccountInfo.component.js
@@ -22,13 +22,28 @@ export default class AccountInfo extends Component {
 
   constructor(props){
     super(props)
+    this.onClickLogout = this.onClickLogout.bind(this)
+  }
+
+  onClickLogout(){
+    fetch("/api/user/signout", {
+      method: "POST",
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      }
+    })
+      .then(res=>res.json())
+      .then(res=>{
+        window.location = "/"    
+      })
   }
 
   render() {
     return (
       <div className="container" style={ style.container }>
         <div>{this.props.userId}</div>
-        <div>로그아웃</div>
+        <div onClick={this.onClickLogout}>로그아웃</div>
         <div>회원탈퇴</div>
       </div>
     )

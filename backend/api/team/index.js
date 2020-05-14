a17f59548c2e6e1d12230f194d1ea6ec14b3af72
owlsogul <owlsogul@naver.com>
2020-05-14 15:24:58 +0900

    team api

--- backend/api/team/index.js
+++ backend/api/team/index.js
@@ -8,4 +8,16 @@ router.get("/list", [serviceUser.checkLogin, tc.listTeam])
 
 router.post("/create", [serviceUser.checkLogin, tc.createTeam])
 
+router.post("/invite", (req, res, next)=>{
+  res.json({ msg: "success"})
+})
+
+router.post("/join", (req, res, next)=>{
+  res.json({ msg: "success"})
+})
+
+router.post("/quit", (req, res, next)=>{
+  res.json({ msg: "success"})
+})
+
 module.exports = router

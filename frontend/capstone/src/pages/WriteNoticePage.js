62cbab50d24cf0f590b26168834590fa75bc55ea
owlsogul <owlsogul@naver.com>
2020-05-18 05:15:45 +0900

    axios 사용 안함

--- frontend/capstone/src/pages/WriteNoticePage.js
+++ frontend/capstone/src/pages/WriteNoticePage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import WriteNotice from '../components/WriteNotice.component'
 import '../App.css'
-import axios from 'axios'
 
 
 
@@ -10,30 +9,30 @@ class WriteNoticePage extends Component {
         super(props)
     }
 
-    submitNotice = async function () {
-        const title = document.getElementsByName('title')[0].nodeValue.trim()
-        const contents = document.getElementsByName('contents')[0].nodeValue.trim()
+    // submitNotice = async function () {
+    //     const title = document.getElementsByName('title')[0].nodeValue.trim()
+    //     const contents = document.getElementsByName('contents')[0].nodeValue.trim()
 
-        if (title === "") {
-            return alert('제목을 입력해주세요.')
-        }
+    //     if (title === "") {
+    //         return alert('제목을 입력해주세요.')
+    //     }
 
-        else if (contents === "") {
-            return alert("내용을 입력해주세요.")
-        }
+    //     else if (contents === "") {
+    //         return alert("내용을 입력해주세요.")
+    //     }
 
-        if(res.data) {
-            alert('공지가 등록되었습니다.')
-            return window.location.replace('/')
-        }
+    //     if(res.data) {
+    //         alert('공지가 등록되었습니다.')
+    //         return window.location.replace('/')
+    //     }
 
-        const data = { title: title, contents: contents }
-        const res = await axios('/writenotice', {
-            method: 'POST',
-            data: data,
-            headers: new Headers()
-        })
-    }
+    //     const data = { title: title, contents: contents }
+    //     const res = await axios('/writenotice', {
+    //         method: 'POST',
+    //         data: data,
+    //         headers: new Headers()
+    //     })
+    // }
     
 
     render() {

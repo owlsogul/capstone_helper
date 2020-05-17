2c8b338da842eab13058bbfc67d7bb4f892683cd
owlsogul <owlsogul@naver.com>
2020-05-17 20:59:27 +0900

    팀 리스트

--- backend/api/team/index.js
+++ backend/api/team/index.js
@@ -4,9 +4,10 @@ const router = express.Router();
 const tc = require("./team.controller")
 const serviceUser = require("../../middleware/serviceuser")
 
-router.get("/list", [serviceUser.checkLogin, tc.listTeam])
+router.post("/list", [serviceUser.checkLogin, tc.listTeam])
 
 router.post("/create", [serviceUser.checkLogin, tc.createTeam])
+
 
 router.post("/invite", (req, res, next)=>{
   res.json({ msg: "success"})

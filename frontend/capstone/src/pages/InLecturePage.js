7c48190ce527073b4e365a6c267fee65cee2e2fb
owlsogul <owlsogul@naver.com>
2020-06-01 09:34:21 +0900

    Revert "Update InLecturePage.js"
    This reverts commit 0a09ea17566e08b9f5158bddccaa41c1c5af1bee.

--- frontend/capstone/src/pages/InLecturePage.js
+++ frontend/capstone/src/pages/InLecturePage.js
@@ -55,8 +55,7 @@ export default class InLecturePage extends Component {
   createWebRTCSocket(){
     return new Promise((res, rej)=>{
       //this.socket = io("http://localhost:30081")
-      //this.socket = io.connect("https://caphelper.owlsogul.com/socket.io")
-      this.socket = io.connect("https://caphelper.owlsogul.com")
+      this.socket = io.connect("https://caphelper.owlsogul.com/socket.io")
       this.setState({ error: "연결 대기중입니다."})
       this.socket.on("connect", ()=>{
         console.log("client nsp->%s", this.socket.nsp);  

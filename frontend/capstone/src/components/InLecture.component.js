8544c303a1b8db7799aed18f753d6a1c63655f71
owlsogul <owlsogul@naver.com>
2020-06-08 06:01:28 +0900

    온라인 수업 발표

--- frontend/capstone/src/components/InLecture.component.js
+++ frontend/capstone/src/components/InLecture.component.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import { ListGroup } from "react-bootstrap"
 import Moment from 'react-moment';
+import { 
+  Button, 
+  Nav, 
+  Badge 
+} from "react-bootstrap"
 
 const style = {
 
   videoWrapper: {
     position: "relative",
-    margin: 5
+    margin: 5,
+    flex: "0 0 auto",
   },
 
   video: {
-    width: 300,
-    height: 300
+    width: 150,
+    height: 100
   },
 
   videoContainer: {
@@ -31,7 +37,8 @@ const style = {
   videoData: {
     position: "absolute", right: 0, top: 0, left: 0, bottom: 0,
     background: "rgba(0,0,0,0.8)", color: "#ffffff",
-    display: "flex", flexDirection: "column", justifyContent: "center", alignContent: "center"
+    display: "flex", flexDirection: "column", justifyContent: "center", alignContent: "center",
+
   }
 }
 
@@ -40,6 +47,49 @@ class ErrorPage extends Component {
     return(
       <div>
         {this.props.msg}
+      </div>
+    )
+  }
+}
+
+class MainVideo extends Component {
+  constructor(props){
+    super(props)
+    this.video = {}
+    this.state = {
+
+    }
+
+    this.handleRef = this.handleRef.bind(this)
+    this.renderVideo = this.renderVideo.bind(this)
+  }
+
+  componentDidUpdate(){
+    if (this.props.stream){
+      this.video.srcObject = this.props.stream.video
+    }
+  }
+
+  handleRef(video){
+    if (video){
+      video.srcObject = this.props.stream.video
+      this.video = video
+    }
+  }
+
+  renderVideo(){
+    if (this.props.stream){
+      return <video ref={this.handleRef} autoPlay style={{ width: "100%", height: "100%"}}/>
+    }
+    else {
+      return <div>No video</div>
+    }
+  }
+
+  render(){
+    return(
+      <div style={{ width: "100%", height: "100%"}}>
+        {this.renderVideo()}
       </div>
     )
   }
@@ -196,12 +246,63 @@ class LecturePage extends Component {
     )
   }
 
+  renderMain(){
+    if (!this.props.userMap || !this.props.userDataMap) {
+      return <div> 데이터를 불러오는 중입니다. </div>
+    }
+    
+    // 발표자 없으면 교수님 찾기
+    let presentationUserId = this.props.presentationUserId
+    let professorUserId = false
+    console.log("userMap", this.props.userMap)
+    Object.entries(this.props.userDataMap).forEach(([key, value])=>{
+      if (value.level == 101) professorUserId = key
+    })
+    if (!professorUserId) { // 아마 발생하지 않을 일.
+      return <div> 교수님이 수업에 없습니다. </div>
+    }
+
+    // socket id 찾기
+    let presentationSocketId = false
+    let professorSocketId = false
+    Object.entries(this.props.userMap).forEach(([key, value])=>{
+      if (value == presentationUserId) presentationSocketId = key
+      if (value == professorUserId) professorSocketId = key
+    })
+    if (!presentationSocketId && !professorSocketId) {
+      return <div> 발표자와 교수님이 안계십니다. </div>
+    }
+
+    let mainSocketId = presentationSocketId ? presentationSocketId : professorSocketId
+    let videoStream = this.state.videos[mainSocketId]
+    return (
+      <MainVideo stream={videoStream} />
+    )
+  }
+
   render(){
     return(
-      <div style={{ display: "flex", flexWrap:"wrap"}}>
-        {
-          Object.entries(this.state.videos).map(this.renderMapping)
-        }
+      <div style={{ position: "relative", flex: "0 1 auto", display: "flex",  flexDirection: "column", height: "100%"}}>
+        
+        {/* user cam part */}
+        <div style={{ overflowX: "scroll", WebkitOverflowScrolling: "touch" }}>
+          <div style={{ display: "flex", flexFlow: "row nowrap",  alignItems: "flex-start", width: 0 }}>
+            {
+              Object.entries(this.state.videos).map(this.renderMapping)
+            }
+          </div>
+        </div>
+
+        {/* main cam part */}
+        <div style={{ flex: "1 1 auto", display: "flex",  flexDirection: "column", justifyContent: "center", alignContent: "center"}}>
+          {this.renderMain()}
+        </div>
+
+        {/* controller part */}
+        <div style={{ position:"absolute", left: 0, right:0, bottom: 0, height: "auto", background:"#000000", padding: 10 }}>
+          <Button variant="primary" size="lg" onClick={this.props.onClickPresentation}>{ this.props.presentationUserId ? "발표종료" : "발표시작" }</Button>
+        </div>
+        
       </div>
     )
   }
